5dc2800ffa59ef0ecead1e77347c7cba4ad58dc2
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-16 22:42:25 -0700

    Add project files.

---
+++ CallingMethodsAssignment/MathOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    
+        public class MathOperations
+        {
+            // Method 1 - addition
+            public int AddTen(int number)
+            {
+                return number + 10;
+            }
+
+            // Method 2 - subtration
+            public int SubtractFive(int number)
+            {
+                return number - 5;
+            }
+
+            // Method 3 - multiply
+            public int MultiplyByThree(int number)
+            {
+                return number * 3;
+            }
+        }
+
+    }
+
---
+++ CallingMethodsAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    
+
+    class Program
+            {
+                static void Main()
+                {
+                    // get user input
+                    Console.Write("Enter a number for me please: ");
+                    int userInput = Convert.ToInt32(Console.ReadLine());
+
+                    // instance for class
+                    MathOperations operations = new MathOperations();
+
+                    // methods
+                    int result1 = operations.AddTen(userInput);
+                    Console.WriteLine($"Adding 10 to your number gives: {result1}");
+
+                    int result2 = operations.SubtractFive(userInput);
+                    Console.WriteLine($"Subtracting 5 from your number gives: {result2}");
+
+                    int result3 = operations.MultiplyByThree(userInput);
+                    Console.WriteLine($"Multiplying your number by 3 gives: {result3}");
+                    Console.ReadLine();
+                }
+            }
+
+    
+}
+
